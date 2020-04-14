e84236d97099906134bbad8a651b922761845258
Shri Sukhani <shridharsukhani@fb.com>
2020-04-13 18:11:36 -0700

    Coding Challenge 2

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -15,3 +15,25 @@ function computeBMI(person) {
 [mark, john].forEach(function(dude) {
     console.log(computeBMI(dude));
 });
+
+var johnsTeamsPoints = [89, 120, 103];
+var mikesTeamsPoints = [116, 94, 123];
+
+var johnsTeamsAverage = 0;
+var mikesTeamsAverage = 0;
+
+for (var i = 0; i < 3; i ++) {
+    johnsTeamsAverage += johnsTeamsPoints[i];
+    mikesTeamsAverage += mikesTeamsPoints[i];
+}
+
+switch (true) {
+    case johnsTeamsAverage > mikesTeamsAverage:
+        console.log("John's teeam won!");
+        break;
+    case mikesTeamsAverage > johnsTeamsAverage:
+        console.log("Mike's team won!");
+        break;
+    default:
+        console.log("It was a draw. :(");
+}
